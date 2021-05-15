f50b08bf9f03428796d07b15022562f762d7384a
Mihail Yonchev <myonchev99@gmail.com>
2021-05-15 20:10:09 +0300

    Implemented localStorage for active status

--- src/components/Rectangle.js
+++ src/components/Rectangle.js
@@ -1,13 +1,19 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 export const Rectangle = ({rect}) => {
 
-    const [active, setActive] = useState(false)
+    const [active, setActive] = useState(localStorage.getItem(`rect-${rect.id}-active`) || false)
+
+    const onActiveStatusChange = (active) => {
+        localStorage.setItem(`rect-${rect.id}-active`, active);
+        setActive(active);
+    }
+
 
     return (
 
-        <div className={`grid-element ${active && 'grid-element--active'}`} onClick={() => setActive(!active)}>
+        <div className={`grid-element ${active && 'grid-element--active'}`} onClick={() => onActiveStatusChange(!active)}>
             <div
             style={{backgroundColor: rect.color}}
             className={`rect rect-${rect.size} `}>
